3929ea9682f3555f8db6fcdfcd1d3011b0c666d6
Jasroop <97998697+jasroopdhingra@users.noreply.github.com>
2025-11-13 13:33:18 -0500

    Ensure homepage router mounts

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,16 @@
 import './index.css';
-import React from "react";
-import { render } from "react-dom";
-import { App } from "./App";
-render(<App />, document.getElementById("root"));
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { AppRouter } from './AppRouter';
+
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error('Root element not found');
+}
+
+createRoot(rootElement).render(
+  <React.StrictMode>
+    <AppRouter />
+  </React.StrictMode>
+);
